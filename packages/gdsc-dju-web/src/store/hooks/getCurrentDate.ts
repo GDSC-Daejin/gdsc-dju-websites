7a72a599f8c52e9db00cca8857823d54e7b60f7c
HyeokE <jhjeong00@gmail.com>
2022-07-16 23:00:24 +0900

    chore: apply gitignore

--- packages/gdsc-dju-web/src/store/hooks/getCurrentDate.ts
+++
@@ -1,7 +0,0 @@
-export const getCurrentDate = () => {
-  const today = new Date();
-  const year = today.getFullYear();
-  const month = today.getMonth() + 1;
-  const day = today.getDate();
-  return `${year}.${month}.${day}`;
-};
