024a0d4c67a498f5c174c9c4ddd774950ca3ed53
HyeokE <jhjeong00@gmail.com>
2022-07-25 21:00:48 +0900

    chore: modify token service

--- packages/gdsc-dju-admin/src/apis/TokenService.ts
+++ packages/gdsc-dju-admin/src/apis/TokenService.ts
@@ -4,7 +4,7 @@ import { Api } from './index';
 class TokenService extends Api {
   getRedirectURL() {
     const OAUTH2_REDIRECT_URI_Dev = 'http://localhost:3000/redirect';
-    const OAUTH2_REDIRECT_URI = 'https://gdsc-dju-admin.web.app/redirect';
+    const OAUTH2_REDIRECT_URI = 'https://gdsc-dju-admin.web.app';
     return `${this.ACCOUNT_API}/oauth2/authorization/google?redirect_uri=${
       process.env.NODE_ENV === 'development'
         ? OAUTH2_REDIRECT_URI_Dev
