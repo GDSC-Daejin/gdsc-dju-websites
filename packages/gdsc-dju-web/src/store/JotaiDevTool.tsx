e512a2016ca944f4ec4a05d5c6865bc31690cfff
HyeokE <jhjeong00@gmail.com>
2022-08-10 01:25:53 +0900

    chore: modify jotai devtool

--- packages/gdsc-dju-web/src/store/JotaiDevTool.tsx
+++ packages/gdsc-dju-web/src/store/JotaiDevTool.tsx
@@ -1,10 +1,12 @@
 import { useAtomsDevtools } from 'jotai/devtools';
 import React, { ReactElement } from 'react';
 
-//Jotai에서 redux devtools를 사용할 수 있게 해줍니다.
+//gJotai에서 redux devtools를 사용할 수 있게 해줍니다.
+const isProduction = process.env.NODE_ENV !== 'production';
 export const AtomDevtools: React.FC<{
   children: ReactElement;
 }> = ({ children }) => {
-  useAtomsDevtools('atomDevtools', { enabled: true });
+  useAtomsDevtools('atomDevtools', { enabled: !isProduction });
+
   return children;
 };
