c15130e3c85de5cab92d0049cd24a3f74a87f452
HyeokE <jhjeong00@gmail.com>
2022-08-09 23:49:34 +0900

    refactor: remove recoil in package.json

--- packages/gdsc-dju-web/src/main.tsx
+++ packages/gdsc-dju-web/src/main.tsx
@@ -1,30 +1,27 @@
 import React from 'react';
-import { HelmetProvider } from 'react-helmet-async';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import { RecoilRoot } from 'recoil';
+import { HelmetProvider } from 'react-helmet-async';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 import { AtomDevtools } from './store/JotaiDevTool';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <RecoilRoot>
-        <AtomDevtools>
-          <HelmetProvider>
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                flex: '1 0 auto',
-                minHeight: '100vh',
-              }}
-            >
-              <App />
-            </div>
-          </HelmetProvider>
-        </AtomDevtools>
-      </RecoilRoot>
+      <AtomDevtools>
+        <HelmetProvider>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              flex: '1 0 auto',
+              minHeight: '100vh',
+            }}
+          >
+            <App />
+          </div>
+        </HelmetProvider>
+      </AtomDevtools>
     </BrowserRouter>
   </React.StrictMode>,
 );
