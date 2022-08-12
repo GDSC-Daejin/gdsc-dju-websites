52c8b7eccf2968254e22c06456772c0956e13736
HyeokE <jhjeong00@gmail.com>
2022-08-13 01:13:22 +0900

    chore: modify vite.config

--- packages/gdsc-dju-web/vite.config.ts
+++ packages/gdsc-dju-web/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
       '@layout': path.resolve(__dirname, './src/components/layout'),
       '@common': path.resolve(__dirname, './src/components/common'),
       '@utils': path.resolve(__dirname, './src/utils'),
-      '@animations': path.resolve(__dirname, '.src/components/animations'),
+      '@animations': path.resolve(__dirname, './src/components/animations'),
       '@pages': path.resolve(__dirname, '.src/pages'),
       '@styles': path.resolve(__dirname, '.src/styles'),
     },
