df1bd6f4dfb22f134c83b923084761840b3e9470
HyeokE <jhjeong00@gmail.com>
2022-08-26 18:46:20 +0900

    refactor: modify getUserData

--- packages/gdsc-dju-blog/src/api/hooks/useGetMyData.ts
+++ packages/gdsc-dju-blog/src/api/hooks/useGetMyData.ts
@@ -1,7 +1,6 @@
 import Cookies from 'js-cookie';
 import { useQuery } from 'react-query';
 import UserService from '../UserService';
-import { getMyToken } from './useGetNewToken';
 
 export const getUserData = async (token: string) => {
   const response = await UserService.getMyData(token);
@@ -16,16 +15,10 @@ export const useGetMyData = () => {
     [`${token}-userdata`],
     () => getUserData(token!),
     {
-      refetchInterval: 20 * 60 * 1000,
       enabled: isEnabled,
       retry: 1,
-      onError: async () => {
-        if (!isEnabled) return;
-        const newToken = await getMyToken(refresh_token, token);
-        await Cookies.set('token', newToken);
-      },
     },
   );
 
-  return { userData: userData ?? userData };
+  return { userData: userData && userData };
 };
