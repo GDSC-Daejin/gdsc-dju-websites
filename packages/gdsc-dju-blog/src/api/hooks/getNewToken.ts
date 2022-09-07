aab4849fd253759bead38aa6a926da1828857e7c
HyeokE <jhjeong00@gmail.com>
2022-09-07 18:04:36 +0900

    feat: add expireDate check

--- packages/gdsc-dju-blog/src/api/hooks/getNewToken.ts
+++ packages/gdsc-dju-blog/src/api/hooks/getNewToken.ts
@@ -11,14 +11,16 @@ const refresh = async (
 ): Promise<AxiosRequestConfig> => {
   const refresh_token = Cookies.get('refresh_token');
   let token = Cookies.get('token');
-  const expires = new Date(new Date().getTime() + 30 * 60 * 1000);
-  if (token && refresh_token) {
-    const response = await TokenService.getRefresh(refresh_token, token);
+  const { expires } = await cookieStore.get('token');
+
+  const expireDate = new Date(new Date().getTime() + 30 * 60 * 1000);
+  if (expires - new Date().getTime() < 0 && refresh_token) {
+    const response = await TokenService.getRefresh(refresh_token, token!);
     if (response.data.header.code === 200) {
       token = response.data.body.data.token;
       Cookies.set('token', token, {
         path: '/',
-        expires: expires,
+        expires: expireDate,
       });
     } else if (response.data.header.code === 403) {
       Cookies.remove('token');
