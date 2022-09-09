1a8c9476034ae48776ce08033c598f64e304b830
HyeokE <jhjeong00@gmail.com>
2022-09-09 16:24:03 +0900

    feat: add blog instance

--- packages/gdsc-dju-blog/src/api/AuthService.ts
+++ packages/gdsc-dju-blog/src/api/AuthService.ts
@@ -6,31 +6,34 @@ const isProduction = process.env.NODE_ENV === 'production';
 const AuthInstance = axios.create({
   timeout: 10000,
   params: {},
-  baseURL: `https://api.gdsc-dju.com/${
-    isProduction ? '/blog-route' : '/blog-route-dev'
-  }`,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
-    'Access-Control-Allow-Headers':
-      'Content-Type, Authorization, Content-Length, X-Requested-With',
-  },
+  baseURL: `https://api.gdsc-dju.com/`,
 });
 AuthInstance.interceptors.request.use(refresh, refreshErrorHandler);
+AuthInstance.defaults.withCredentials = true;
+
+const AuthBlogInstance = axios.create({
+  timeout: 10000,
+  params: {},
+  baseURL: `https://api.gdsc-dju.com/${
+    isProduction ? 'blog-route' : 'blog-route-dev'
+  }`,
+});
+AuthBlogInstance.interceptors.request.use(refresh, refreshErrorHandler);
 AuthInstance.defaults.withCredentials = true;
 
 const Instance = axios.create({
   timeout: 10000,
   params: {},
-  baseURL: `https://api.gdsc-dju.com/`,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
-    'Access-Control-Allow-Headers':
-      'Content-Type, Authorization, Content-Length, X-Requested-With',
-  },
+  baseURL: `https://api.gdsc-dju.com`,
+});
+const BlogInstance = axios.create({
+  timeout: 10000,
+  params: {},
+  baseURL: `https://api.gdsc-dju.com/${
+    isProduction ? 'blog-route' : 'blog-route-dev'
+  }`,
 });
 Instance.interceptors.request.use();
 Instance.defaults.withCredentials = true;
 
-export { AuthInstance, Instance };
+export { Instance, AuthInstance, BlogInstance, AuthBlogInstance };
--- packages/gdsc-dju-blog/src/api/PostService.ts
+++ packages/gdsc-dju-blog/src/api/PostService.ts
@@ -5,56 +5,64 @@ import {
   RowScrapList,
 } from '@type/postData';
 import Cookies from 'js-cookie';
-import { AuthInstance, Instance } from './AuthService';
+import { AuthBlogInstance, BlogInstance } from './AuthService';
 
 class PostService {
   //전체 포스트
   getPostData = (postId: string) => {
-    return AuthInstance.get<RowPostDataType>(`/api/v1/post/${postId}`);
+    return AuthBlogInstance.get<RowPostDataType>(`/api/v1/post/${postId}`);
   };
   getPostsData = (params: string) => {
-    return Instance.get<RowPostListType>(`/api/v1/post/list${params}`);
+    return BlogInstance.get<RowPostListType>(`/api/v1/post/list${params}`);
   };
   //내 포스트
   getMyPostData = (postId: string) => {
-    return AuthInstance.get<RowPostDataType>(`/api/guest/v1/myPost/${postId}`);
+    return AuthBlogInstance.get<RowPostDataType>(
+      `/api/guest/v1/myPost/${postId}`,
+    );
   };
   getMyPostsData = (params: string) => {
-    return AuthInstance.get<RowPostListType>(`/api/guest/v1/myPost/${params}`);
+    return AuthBlogInstance.get<RowPostListType>(
+      `/api/guest/v1/myPost/${params}`,
+    );
   };
 
   getMyPostsTempData = (params: string) => {
-    return AuthInstance.get<RowPostListType>(
+    return AuthBlogInstance.get<RowPostListType>(
       `/api/guest/v1/myPost/temp${params}`,
     );
   };
   getMyPostsNotTempData = (params: string) => {
-    return AuthInstance.get<RowPostListType>(
+    return AuthBlogInstance.get<RowPostListType>(
       `/api/guest/v1/myPost/notTemp${params}`,
     );
   };
   getUserPostsNotTempData = (userId: string, params: string) => {
-    return Instance.get<RowPostListType>(`/api/v1/${userId}/notTemp/${params}`);
+    return BlogInstance.get<RowPostListType>(
+      `/api/v1/${userId}/notTemp/${params}`,
+    );
   };
   postMyPostData = (postData: PostPostDataType) => {
-    return AuthInstance.post(`/api/member/v2/post`, postData);
+    return AuthBlogInstance.post(`/api/member/v2/post`, postData);
   };
   updateMyPostData = (postData: PostPostDataType, postId: string) => {
-    return AuthInstance.put(`/api/member/v2/post/${postId}`, postData);
+    return AuthBlogInstance.put(`/api/member/v2/post/${postId}`, postData);
   };
   deleteMyPostData = (postId: number) => {
-    return AuthInstance.delete(`/api/member/v2/post/${postId}`);
+    return AuthBlogInstance.delete(`/api/member/v2/post/${postId}`);
   };
   getMyScrapList = () => {
-    return AuthInstance.get<RowScrapList>(`/api/guest/v1/myScrap/list`);
+    return AuthBlogInstance.get<RowScrapList>(`/api/guest/v1/myScrap/list`);
   };
   getMyScrapData = (params: string) => {
-    return AuthInstance.get<RowPostListType>(`/api/guest/v1/myScrap${params}`);
+    return AuthBlogInstance.get<RowPostListType>(
+      `/api/guest/v1/myScrap${params}`,
+    );
   };
 
   updateMyScrapData = (postId: number) => {
     const token = Cookies.get('token');
-    return AuthInstance.post(`/api/guest/v1/scrap/${postId}`, {
+    return AuthBlogInstance.post(`/api/guest/v1/scrap/${postId}`, {
       authorities: [
         {
           authority: `Bearer ${token}`,
@@ -71,7 +79,7 @@ class PostService {
     category: string;
     page: number;
   }) => {
-    return Instance.get(
+    return BlogInstance.get(
       `/api/v1/post/search/${searchContent}?page=${page}&size=${16}`,
     );
   };
