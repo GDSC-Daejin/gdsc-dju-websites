46805cd2abd7bc099a7de6e15583dc8f9fb1e5ff
HyeokE <jhjeong00@gmail.com>
2022-10-05 16:35:08 +0900

    chore: modify redirect

--- packages/gdsc-dju-admin/src/pages/Redirect/index.tsx
+++ packages/gdsc-dju-admin/src/pages/Redirect/index.tsx
@@ -1,19 +1,20 @@
 import Cookies from 'js-cookie';
 import React, { useEffect } from 'react';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useGetMyData } from '../../apis/hooks/useGetMyData';
 import { GoogleSpinnerStatic } from '../../components/Lottie/GoogleSpinner';
 
 const Redirect = () => {
   const navigate = useNavigate();
-  const [searchParams] = useSearchParams();
   const token = Cookies.get('token');
 
   const { userData } = useGetMyData();
 
   useEffect(() => {
     (async function () {
-      if (token) {
+      if (!token) return;
+      if (!(userData && userData.role)) return;
+      if (userData.role === 'LEAD' || userData.role === 'CORE') {
         await navigate('/certified');
       }
     })();
