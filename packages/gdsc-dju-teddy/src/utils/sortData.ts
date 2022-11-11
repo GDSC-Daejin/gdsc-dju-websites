cf73aebbc541bfe758d230aa6345de6a6961bcf1
HyeokE <jhjeong00@gmail.com>
2022-11-11 16:22:35 +0900

    chore: fix sortData func

--- packages/gdsc-dju-teddy/src/utils/sortData.ts
+++ packages/gdsc-dju-teddy/src/utils/sortData.ts
@@ -1,8 +1,15 @@
 export function dataSort<T extends Record<any, any>>(
   data: T[],
   filter: keyof T extends string ? keyof T : never,
+  order: 'asc' | 'desc' = 'desc',
 ) {
   return data.sort((a, b) => {
-    return a[filter] - b[filter];
+    if (a[filter] < b[filter]) {
+      return order === 'asc' ? -1 : 1;
+    }
+    if (a[filter] > b[filter]) {
+      return order === 'asc' ? 1 : -1;
+    }
+    return 0;
   });
 }
