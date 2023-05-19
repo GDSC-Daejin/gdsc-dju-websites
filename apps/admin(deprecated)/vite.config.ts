ac586e8bd45453d08892ad629cbdc7b418fc6268
HyeokE <jhjeong00@gmail.com>
2023-05-19 20:14:05 +0900

    fix: remove https

--- apps/admin(deprecated)/vite.config.ts
+++ apps/admin(deprecated)/vite.config.ts
@@ -1,17 +1,17 @@
 import path from 'path';
 
 import { defineConfig } from 'vite';
-import mkcert from 'vite-plugin-mkcert';
+// import mkcert from 'vite-plugin-mkcert';
 
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [react()],
   server: {
-    host: 'devlog.gdsc-dju.com',
+    // host: 'devlog.gdsc-dju.com',
     port: 3000,
-    https: true,
+    // https: true,
   },
   optimizeDeps: { include: ['firebase/app', 'firebase/firestore'] },
   resolve: {
