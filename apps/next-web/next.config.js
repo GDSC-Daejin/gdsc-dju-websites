379afc15d51393687d758d0c5223e1056bc12d90
HyeokE <jhjeong00@gmail.com>
2023-07-08 23:30:07 +0900

    fix: remove next-web

--- apps/next-web/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  typescript: { ignoreBuildErrors: true },
-};
-
-module.exports = nextConfig;
