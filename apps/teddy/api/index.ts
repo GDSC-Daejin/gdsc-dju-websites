8573ca68516a8e016f32e18feb77cbbccadd1a4b
정우성 <dntjd1097@gmail.com>
2023-08-12 19:38:20 +0900

    chore : eidt api url (#186)

--- apps/teddy/api/index.ts
+++ apps/teddy/api/index.ts
@@ -6,7 +6,7 @@ import { FilterType, ListType } from '../types/filterType';
 export class TeddyBearApi {
   protected API: string;
   constructor() {
-    this.API = 'https://gdsc-teddy-boo105.koyeb.app';
+    this.API = 'https://port-0-gdsc-teddy-bear-backend-5llo2alhev61it.sel4.cloudtype.app';
   }
   getScoreboard = (filter: FilterType, listType: ListType) => {
     return axios.get<rowScoreBoardType>(
