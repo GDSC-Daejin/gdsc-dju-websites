9699433b8335d1448b3d63d2bd3e92cb7fd08906
HyeokE <jhjeong00@gmail.com>
2023-08-29 19:54:50 +0900

    fix: conflicts

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,25 +4,23 @@ import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-import { AtomDevtools } from './store/JotaiDevTool';
+// import { AtomDevtools } from './store/JotaiDevTool';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AtomDevtools>
-        <HelmetProvider>
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              flex: '1 0 auto',
-              minHeight: '100vh',
-            }}
-          >
-            <App />
-          </div>
-        </HelmetProvider>
-      </AtomDevtools>
+      <HelmetProvider>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            flex: '1 0 auto',
+            minHeight: '100vh',
+          }}
+        >
+          <App />
+        </div>
+      </HelmetProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
