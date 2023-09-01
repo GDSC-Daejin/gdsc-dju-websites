1a035b224638c499fe231a818bca74c44729e39b
정우성 <dntjd1097@gmail.com>
2023-09-01 12:08:46 +0900

    feature : 3rd member list (#190)

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,13 +1,14 @@
 import * as path from 'path';
 
 import { defineConfig } from 'vite';
-import mkcert from 'vite-plugin-mkcert';
+// import mkcert from 'vite-plugin-mkcert';
 
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  // plugins: [react(), mkcert()],
+  plugins: [react()],
   server: {
     // host: 'devlog.gdsc-dju.com',
     port: 3000,
