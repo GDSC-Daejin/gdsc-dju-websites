dff2176399499bae2106207c2fc0a9cac0291a64
dntjd1097 <dntjd1097@gmail.com>
2023-09-22 05:04:40 +0900

    chore : 배포용 setting

--- apps/admin(deprecated)/vite.config.ts
+++ apps/admin(deprecated)/vite.config.ts
@@ -1,38 +1,13 @@
 import path from 'path';
 
 import { defineConfig } from 'vite';
-// local로 돌리때 주석 지우기
-import mkcert from 'vite-plugin-mkcert';
-
 import react from '@vitejs/plugin-react';
+
 /* 배포할때 주석 해제하기*/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 3000,
-//   },
-//   optimizeDeps: { include: ['firebase/app', 'firebase/firestore'] },
-//   resolve: {
-//     alias: {
-//       '@src': path.resolve(__dirname, './src'),
-//       '@type': path.resolve(__dirname, './src/types'),
-//       '@assets': path.resolve(__dirname, './src/assets'),
-//       '@layout': path.resolve(__dirname, './src/components/layout'),
-//       '@common': path.resolve(__dirname, './src/components/common'),
-//       '@utils': path.resolve(__dirname, './src/utils'),
-//       '@animations': path.resolve(__dirname, './src/components/animations'),
-//       '@pages': path.resolve(__dirname, './src/pages'),
-//     },
-//   },
-// });
-/* 로컬용*/
 export default defineConfig({
-  plugins: [react(), mkcert()],
-  // plugins: [react()],
+  plugins: [react()],
   server: {
-    host: 'dev.gdscdju.dev',
     port: 3000,
-    https: true,
   },
   optimizeDeps: { include: ['firebase/app', 'firebase/firestore'] },
   resolve: {
@@ -48,3 +23,27 @@ export default defineConfig({
     },
   },
 });
+/* 로컬용*/
+// import mkcert from 'vite-plugin-mkcert';
+// export default defineConfig({
+//   plugins: [react(), mkcert()],
+//   // plugins: [react()],
+//   server: {
+//     host: 'dev.gdscdju.dev',
+//     port: 3000,
+//     https: true,
+//   },
+//   optimizeDeps: { include: ['firebase/app', 'firebase/firestore'] },
+//   resolve: {
+//     alias: {
+//       '@src': path.resolve(__dirname, './src'),
+//       '@type': path.resolve(__dirname, './src/types'),
+//       '@assets': path.resolve(__dirname, './src/assets'),
+//       '@layout': path.resolve(__dirname, './src/components/layout'),
+//       '@common': path.resolve(__dirname, './src/components/common'),
+//       '@utils': path.resolve(__dirname, './src/utils'),
+//       '@animations': path.resolve(__dirname, './src/components/animations'),
+//       '@pages': path.resolve(__dirname, './src/pages'),
+//     },
+//   },
+// });
