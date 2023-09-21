701bf7b8946fbe08c0b8e8a0d51fa6af38456438
정우성 <dntjd1097@gmail.com>
2023-09-21 21:26:32 +0900

    Feature/member permission/admin (#199)

--- apps/admin(deprecated)/src/components/common/modal/MemberInfoModal/index.tsx
+++ apps/admin(deprecated)/src/components/common/modal/MemberInfoModal/index.tsx
@@ -7,11 +7,19 @@ import {
   ButtonWrapper,
   ModalBackgroundWrapper,
   ModalInner,
+  ModalContainer,
+  ModalInnerNav,
+  ModalInnerBody,
+  ModalTitle,
+  ModalInnerGridContainer,
+  ModalInnerGridItem,
+  ModalP,
 } from '@common/modal/styled';
 import { putMemberData } from '@src/apis/UserService';
 import { useModalHandle } from '@src/hooks/useModalHandle';
 import { IUserDataType } from '@type/userDataType';
 import OutsideClickHandler from '@utils/OutsideClickHandler';
+import { ClearButton } from '@common/ModalButton';
 
 type Props = {
   selectMember: IUserDataType | null;
@@ -19,6 +27,7 @@ type Props = {
 const MemberInfoModal = (selectMember: Props) => {
   const { modal, closeModal } = useModalHandle('MEMBER');
 
+  /** Core로 할당 */
   const GiveAdmin = async () => {
     if (selectMember.selectMember) {
       await putMemberData({
@@ -28,6 +37,7 @@ const MemberInfoModal = (selectMember: Props) => {
       window.location.reload();
     }
   };
+  /** Member로 할당 */
   const GiveMember = async () => {
     if (selectMember.selectMember) {
       await putMemberData({
@@ -37,7 +47,16 @@ const MemberInfoModal = (selectMember: Props) => {
       window.location.reload();
     }
   };
-
+  /** Guest로 할당 */
+  const GiveGuest = async () => {
+    if (selectMember.selectMember) {
+      await putMemberData({
+        userId: selectMember.selectMember?.userId,
+        role: 'GUEST',
+      });
+      window.location.reload();
+    }
+  };
   return (
     <AnimatePresence>
       {modal.isOpen && (
@@ -52,22 +71,68 @@ const MemberInfoModal = (selectMember: Props) => {
             }}
           >
             <ModalInner>
+              <ModalInnerNav>
+                <ModalTitle>멤버수정</ModalTitle>
+                <ClearButton onClick={closeModal} />
+              </ModalInnerNav>
+              <ModalInnerBody>
+                <ModalInnerGridContainer>
+                  <ModalInnerGridItem>
+                    <ModalP>Name</ModalP>
+                  </ModalInnerGridItem>
+                  <ModalInnerGridItem>
+                    <ModalP>{selectMember.selectMember?.username}</ModalP>
+                  </ModalInnerGridItem>
+                  <ModalInnerGridItem>
+                    <ModalP>E-mail</ModalP>
+                  </ModalInnerGridItem>
+                  <ModalInnerGridItem>
+                    <ModalP>{selectMember.selectMember?.email}</ModalP>
+                  </ModalInnerGridItem>
+
+                  <ModalInnerGridItem>
+                    <ModalP>Permission</ModalP>
+                  </ModalInnerGridItem>
+                  <ModalInnerGridItem>
+                    <ModalP> {selectMember.selectMember?.role}</ModalP>
+                  </ModalInnerGridItem>
+                  {selectMember.selectMember?.memberInfo.positionType && (
+                    <>
+                      <ModalInnerGridItem>
+                        <ModalP>Position</ModalP>
+                      </ModalInnerGridItem>
+                      <ModalInnerGridItem>
+                        <ModalP>
+                          {' '}
+                          {selectMember.selectMember?.memberInfo.positionType}
+                        </ModalP>
+                      </ModalInnerGridItem>
+                    </>
+                  )}
+                </ModalInnerGridContainer>
+              </ModalInnerBody>
               <ButtonWrapper>
-                <GDSCButton
-                  text={'Core'}
-                  color={'blue900'}
-                  onClick={GiveAdmin}
-                />
-                <GDSCButton
-                  text={'Member'}
-                  color={'red600'}
-                  onClick={GiveMember}
-                />
-                <GDSCButton
-                  text={'뒤로가기'}
-                  color={'grey400'}
-                  onClick={closeModal}
-                />
+                {selectMember.selectMember?.role !== 'CORE' && (
+                  <GDSCButton
+                    text={'Core'}
+                    color={'blue900'}
+                    onClick={GiveAdmin}
+                  />
+                )}
+                {selectMember.selectMember?.role !== 'MEMBER' && (
+                  <GDSCButton
+                    text={'Member'}
+                    color={'red600'}
+                    onClick={GiveMember}
+                  />
+                )}
+                {selectMember.selectMember?.role !== 'GUEST' && (
+                  <GDSCButton
+                    text={'GUEST'}
+                    color={'red600'}
+                    onClick={GiveGuest}
+                  />
+                )}
               </ButtonWrapper>
             </ModalInner>
           </OutsideClickHandler>
